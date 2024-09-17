af045d427ed81a39691f6bec8e2334c5a5e4b091
Hunter Keca <hunterkeca@gmail.com>
2024-09-16 19:15:33 -0700

    feat: connect broadcast websocket server to event handler callback

--- src/BakkesSocket.cpp
+++ src/BakkesSocket.cpp
@@ -1,6 +1,6 @@
 #include "BakkesSocket.h"
 
-BAKKESMOD_PLUGIN(BakkesSocket, "Websocket", "1.0", PLUGINTYPE_FREEPLAY);
+BAKKESMOD_PLUGIN(BakkesSocket, "BakkesSocket", "1.0.0.0", PLUGINTYPE_THREADED);
 
 std::shared_ptr<CVarManagerWrapper> _globalCvarManager;
 
@@ -17,27 +17,39 @@ struct TickerStruct {
 void BakkesSocket::statEvent(ServerWrapper caller, void* args)
 {
   auto tArgs = (TickerStruct*)args;
+  auto receiver = PriWrapper(tArgs->Receiver);
+  auto victim = PriWrapper(tArgs->Victim);
   auto statEvent = StatEventWrapper(tArgs->StatEvent);
-  auto eventString = statEvent.GetEventName();
+  std::string eventString = statEvent.GetEventName();
 
-  if (eventString == "Goal") {
-    _globalCvarManager->log("Player scored goal");
-  }
+  wsServer->sendMessage(eventString);
+  wsServer->sendMessage(std::format("score {}", receiver.GetMatchScore()));
 }
 
 void BakkesSocket::onLoad()
 {
   _globalCvarManager = cvarManager;
-  cvarManager->log("Loading WebSocket support for bakkesmod...");
 
   gameWrapper->HookEventWithCallerPost<ServerWrapper>(
     "Function TAGame.GFxHUD_TA.HandleStatTickerMessage",
     std::bind(&BakkesSocket::statEvent, this,
       std::placeholders::_1, std::placeholders::_2)
   );
+
+  cvarManager->log("Starting broadcast server");
+  wsServer = new BroadcastServer();
+  wsServer->run(9002);
 }
 
-void BakkesSocket::onUnLoad()
+
+void BakkesSocket::onUnload()
 {
-  //
+  cvarManager->log("Stopping broadcast server and unhooking events");
+
+  if (wsServer) {
+    wsServer->stop();
+    delete wsServer;
+  }
+
+  gameWrapper->UnhookEvent("Function TAGame.GFxHUD_TA.HandleStatTickerMessage");
 }
--- src/BakkesSocket.h
+++ src/BakkesSocket.h
@@ -1,16 +1,19 @@
 #pragma once
 
-#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
-#endif
+#define _CRT_SECURE_NO_WARNINGS
 
 #include "bakkesmod/plugin/bakkesmodplugin.h"
+#include "BroadcastServer.h"
 
 class BakkesSocket: public BakkesMod::Plugin::BakkesModPlugin
 {
 public:
-    virtual void onLoad();
-    virtual void onUnLoad();
+    virtual void onLoad() override;
+    virtual void onUnload() override;
 
     void statEvent(ServerWrapper caller, void* args);
+
+private:
+    BroadcastServer* wsServer;
 };
